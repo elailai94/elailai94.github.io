02cf51c7f470bc3b7fe2eb945ae723904f6295a8
Elisha Lai <elisha.lai@outlook.com>
2017-08-10 01:18:53 -0400

    add travel map

---
+++ src/travelMap/services/Foursquare.js
@@ -0,0 +1,40 @@
+import request from 'request-promise-native';
+
+import Settings from '../constants/Settings';
+
+// Get the options for a request
+function getOptions(baseURL, path, queryStrings) {
+  return {
+    uri: `${baseURL}${path}`,
+    qs: queryStrings,
+    gzip: true,
+    json: true,
+  };
+}
+
+// Call the endpoint
+async function callEndpoint(baseURL, path, queryStrings) {
+  const options = getOptions(baseURL, path, queryStrings);
+  const response = await request(options);
+  
+  return response;
+}
+
+/*
+ * Return a list of all venues visited by the specified user, along
+ * with how many visits and when they were last there
+ */ 
+async function getVenueHistory(accessToken) {
+  const baseURL = Settings.foursquare.API_BASE_URL;
+  const path = Settings.foursquare.VENUE_HISTORY_PATH;
+  const currentDate = '20170809';
+  const queryStrings = {
+    oauth_token: accessToken,
+    v: currentDate,
+  };
+  
+  const response = await callEndpoint(baseURL, path, queryStrings);
+  return response;
+}
+
+export { getVenueHistory };
